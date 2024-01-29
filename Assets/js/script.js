092e07d77cc88c07df1d57439ff3ead35d76f45b
Vickouma77 <Vickprestone20@gmail.com>
2024-01-29 22:09:49 +0300

    script

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -10,16 +10,3 @@ window.addEventListener("load", function () {
   preloader.classList.add("loaded");
   document.body.classList.add("loaded");
 });
-
-/**
- * 
- * event listeners 
- *  
- * 
- */
-
-const addEventOnElements = function (elements, eventType, callback) {
-    for (let i = 0, len = elements.length; i < len; i++) {
-      elements[i].addEventListener(eventType, callback);
-    }
-  }
