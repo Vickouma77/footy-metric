d0a38f14ab043a47510d37eb5f1370f7124b8574
Vickouma77 <Vickprestone20@gmail.com>
2024-01-29 22:07:23 +0300

    script

---
+++ Assets/js/script.js
@@ -0,0 +1,25 @@
+/**
+ * Preload
+ * 
+ * loading will end when the page is fully loaded
+ */
+
+const preloader = document.querySelector("[data-preaload]");
+
+window.addEventListener("load", function () {
+  preloader.classList.add("loaded");
+  document.body.classList.add("loaded");
+});
+
+/**
+ * 
+ * event listeners 
+ *  
+ * 
+ */
+
+const addEventOnElements = function (elements, eventType, callback) {
+    for (let i = 0, len = elements.length; i < len; i++) {
+      elements[i].addEventListener(eventType, callback);
+    }
+  }
